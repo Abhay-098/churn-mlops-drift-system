8c32f53d68801a9bfc446c52286d5f68bd49597e
Abhay Kumar <abhaykumar.ab219@gmail.com>
2026-03-04 13:01:40 +0530

    Added model evaluation endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from prometheus_client import Counter, generate_latest
 from fastapi.responses import Response
 from app.model import load_model, save_model, get_model_info
@@ -122,3 +123,36 @@ def rollback(version: str):
 @app.get("/metrics")
 def metrics():
     return Response(generate_latest(), media_type="text/plain")
+
+@app.get("/evaluate")
+def evaluate_model():
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA_PATH = os.path.join(BASE_DIR, "data", "churn.csv")
+
+    df = pd.read_csv(DATA_PATH)
+
+    # Preprocess same as training
+    df.drop("customerID", axis=1, inplace=True)
+    df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+    df.dropna(inplace=True)
+    df["Churn"] = df["Churn"].map({"Yes": 1, "No": 0})
+    df = pd.get_dummies(df, drop_first=True)
+
+    X = df.drop("Churn", axis=1)
+    y = df["Churn"]
+
+    # Predict
+    preds = model.predict(X)
+
+    accuracy = accuracy_score(y, preds)
+    precision = precision_score(y, preds)
+    recall = recall_score(y, preds)
+    f1 = f1_score(y, preds)
+
+    return {
+        "accuracy": float(accuracy),
+        "precision": float(precision),
+        "recall": float(recall),
+        "f1_score": float(f1)
+    }
